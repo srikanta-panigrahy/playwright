85dc1630f9aeff7624928ae65dcd062fd7870901
Lavanya-yeturi29 <lavanya.yeturi@innocito.com>
2025-07-03 01:00:26 +0530

    updated

--- tests/UI_specFiles/login.spec.js
+++ tests/UI_specFiles/login.spec.js
@@ -1,14 +1,27 @@
 const { test, expect } = require("@playwright/test");
 const sections = require("../../pageObjects/UI_Pages/pageIndex");
-const uiTestData = require("../../test_Data/testData.json");
+const path = require("path");
 require("dotenv").config();
-test("Log in to the Joulez application using valid credentials", async ({
-  page,
-}) => {
-  const loginPage = new sections.LoginPage(test, page);
-  await loginPage.launchingApplication([process.env.BASE_URL]);
-  await loginPage.logInWithValidCredentials(
-    [process.env.USER_EMAILID],
-    [process.env.PASSWORD]
-  );
+const { readExcelData } = require("../../utilites/readExcel.js");
+
+test.describe("Login Tests", () => {
+  test.only("Login using .env credentials", async ({ page }) => {
+    const loginPage = new sections.LoginPage(test, page);
+    await loginPage.launchingApplication([process.env.BASE_URL]);
+    await loginPage.logInWithValidCredentials(
+      [process.env.USER_EMAILID],
+      [process.env.PASSWORD]
+    );
+  });
+
+  test("Login with Excel data", async ({ page }) => {
+    const excelPath = path.resolve(__dirname, "../../test_Data/userData.xlsx");
+    const testData = readExcelData(excelPath, "Sheet1");
+
+    for (const { Email, Password } of testData) {
+      const loginPage = new sections.LoginPage(test, page);
+      await loginPage.launchingApplication([process.env.BASE_URL]);
+      await loginPage.logInWithValidCredentials([Email], [Password]);
+    }
+  });
 });
--- utilites/readExcel.js
+++ utilites/readExcel.js
@@ -1,6 +1,7 @@
 const xlsx = require("xlsx");
 
 function readExcelData(filePath, sheetName) {
+  console.log(filePath);
   const workbook = xlsx.readFile(filePath);
   const worksheet = workbook.Sheets[sheetName];
   return xlsx.utils.sheet_to_json(worksheet); // returns array of objects
